3dbc6a4070c7fae436c5bfca65ed88dff6d1fe24
RafaelBlanco95 <rafa.blanco.95@gmail.com>
2023-02-08 08:43:50 +0100

    Configuration Projects Added

---
+++ PruebaStarters/src/main/java/com/nttdata/bootcamp/controller/PruebaController.java
@@ -0,0 +1,28 @@
+package com.nttdata.bootcamp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.nttdata.bootcamp.saludoidioma.domain.Saludo;
+@RestController
+public class PruebaController {
+	
+	@Autowired
+	private Saludo saludo;
+	/*
+	@Autowired
+	private Temperatura temperatura;*/
+	
+	@GetMapping(path="/prueba")
+	public String pruebaSaludo() {
+		return saludo.mensajeSaludo();
+	}
+	/*
+	@GetMapping(path="/grados")
+	public String pruebaTemperatura(String degrees) {
+		return Double.toString(temperatura.convert(degrees));
+	}
+	*/
+}
