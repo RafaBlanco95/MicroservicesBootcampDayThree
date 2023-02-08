d6271a202841235bcd8f874c4c37d4aede9abd98
RafaelBlanco95 <rafa.blanco.95@gmail.com>
2023-02-08 12:57:25 +0100

    Config Works

--- PruebaStarters/src/main/java/com/nttdata/bootcamp/controller/PruebaController.java
+++ PruebaStarters/src/main/java/com/nttdata/bootcamp/controller/PruebaController.java
@@ -4,25 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import com.nttdata.bootcamp.saludoidioma.domain.Saludo;
+
 @RestController
 public class PruebaController {
-	
+
 	@Autowired
 	private Saludo saludo;
-	/*
-	@Autowired
-	private Temperatura temperatura;*/
-	
-	@GetMapping(path="/prueba")
+
+	@GetMapping(path = "/prueba")
 	public String pruebaSaludo() {
 		return saludo.mensajeSaludo();
 	}
-	/*
-	@GetMapping(path="/grados")
-	public String pruebaTemperatura(String degrees) {
-		return Double.toString(temperatura.convert(degrees));
+
+	@GetMapping(path = "/helloWorld")
+	public String helloWorld() {
+		//counter.increment();
+		return "hello World";
 	}
-	*/
+
 }
